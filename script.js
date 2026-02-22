73a4abd9eb9582d9ba807713acebc6ee68e44623
haneh938 <66182158+haneh938@users.noreply.github.com>
2026-02-22 00:15:07 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementsByTagName("h1")[0].style.fontSize = "4vw";
+
+document.getElementsByTagName("h2")[0].style.fontSize = "3vw";
+
+document.getElementsByTagName("p")[0].style.fontSize = "3vw";
+
+document.getElementsByTagName("h2")[0].style.fontSize = "2vw";
